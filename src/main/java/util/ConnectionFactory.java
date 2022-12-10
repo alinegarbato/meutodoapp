c95480b44ab4e2dc2144053c82e25bddf3b753b6
bruno <bruno@LAPTOP-CCT3QMHG>
2022-12-10 16:09:48 -0300

    class main

--- src/main/java/util/ConnectionFactory.java
+++ src/main/java/util/ConnectionFactory.java
@@ -9,5 +9,11 @@ package util;
  @author bruno
  */
 public class ConnectionFactory {
-    
+    public static final String DRIVER = "com.mysql.jdbc.Driver";
+    public static final String URL = "jdbc:mysql://localhost:3306/todoapp";
+    public static final String USER = "root";
+    public static final String PASS = "";
+
+
+
 }
